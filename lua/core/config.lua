6a025773c3ef1eb131c3cca3be8c0e94b37146cd
SwissalpS <Luke@SwissalpS.ws>
2020-07-28 14:24:17 +0200

    adding core/config.lua and user/init.lua

---
+++ lua/core/config.lua
@@ -0,0 +1,9 @@
+--[[
+  lua/core/config.lua
+  default configurations for the bot, these can be overriden
+--]]
+local config = {}
+
+
+
+return config
---
+++ lua/user/init.lua
@@ -0,0 +1,10 @@
+--[[
+  lua/user/init.lua
+  user settings and additions
+  core-devs leave this file alone to avoid introducing merge errors for users
+  Users may load other files and add overrides for settings and functions
+  here, example set passwords in here
+--]]
+
+-- return true or something to indicate load worked
+return true
