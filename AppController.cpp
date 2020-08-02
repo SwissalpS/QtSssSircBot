177b0418be12f8c80359fb326b8bcaa52743e592
SwissalpS <Luke@SwissalpS.ws>
2020-08-02 14:08:25 +0200

    better config.json handling

--- AppController.cpp
+++ AppController.cpp
@@ -85,6 +85,7 @@ AppController *AppController::pAppController() {
 
 void AppController::addConnection(const QJsonObject oConfig) {
 
+	QJsonDocument oJdoc(oConfig);
 	IRCclientController *pController = new IRCclientController(oConfig, this);
 	const QString sID = pController->getConnectionID();
 
