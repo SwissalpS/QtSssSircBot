04a414527196fa056d4e47cf2f71e99a0197dcc9
SwissalpS <Luke@SwissalpS.ws>
2020-08-17 05:11:50 +0200

    bugfix: could not load plugin INFO.txt
    now it's an actual lua file, so no more warning :)

---
+++ lua/plugins/info.lua
@@ -0,0 +1,9 @@
+--[[
+    lua/plugins/info.lua
+    dummy plugin to make sure folder is created
+    copy, move or symlink the plugins you want active from plugins_available
+    into this directory: lua/plugins
+--]]
+
+return true
+
