81083791357bf7883747b0ab1ca2741890db86a5
SwissalpS <Luke@SwissalpS.ws>
2022-02-21 19:45:26 +0100

    doc build script
    using lDoc instead of lua2dox filter

---
+++ doc/build.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/sh
+## QtSssSircBot/doc/build.sh
+## script to combine Lua documentation with LDoc into
+## build with Sphinx-chain.
+
+# change into directory where script resides in
+ME=$(readlink -f "$0");
+MEPATH=$(dirname "$ME");
+cd $MEPATH;
+
+# update templated files
+#./updateVersionTags.sh
+
+# remove old LDoc in Sphinx-tree, if exists
+sOutPath=SphinxBreatheExhaleDocutils/source/_static/LDoc;
+if [ -d "$sOutPath" ]; then
+  rm -r $sOutPath;
+fi;
+
+# build the LDoc-umentation
+LDoc/build.sh;
+
+echo "*************************************";
+echo "*************************************";
+echo "*************************************";
+echo "************ LDoc built *************";
+echo "*************************************";
+echo "*************************************";
+echo "*************************************";
+
+# move result to Sphinx-tree
+mv LDoc/html $sOutPath
+
+# start the Sphinx-chain
+SphinxBreatheExhaleDocutils/build.sh;
+
+echo "output is in QtSssSircBot/doc/SphinxBreatheExhaleDocutils/build/html";
+
+# move result to staging
+echo `pwd`;
+#mv SphinxBreatheExhaleDocutils/build/html ../../SwissalpS.github.io/QtSssSircBot/branch${branch}/${version}
